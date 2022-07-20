9510768516b4535089c979564780ceae0428c534
Milos Stankovic <milos.stankovic@swiftylabs.io>
2022-07-20 15:21:13 +0200

    Added Loger

---
+++ Loger/Date+ToString.swift
@@ -0,0 +1,15 @@
+//
+//  Date.swift
+//  SLEssentials
+//
+//  Created by Milos Stankovic on 20.7.22..
+//  Copyright © 2022 SwiftyLabs. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+	var toString: String {
+		"\(self)"
+	}
+}
