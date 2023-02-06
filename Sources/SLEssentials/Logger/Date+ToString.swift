6cd0ef1740777f7ac6b9275dc12ab9f60815b8c1
Slobodan Ristic <slobodan.ristic@swiftylabs.io>
2023-02-06 16:43:43 +0100

    Deleted redundant files

--- Sources/SLEssentials/Logger/Date+ToString.swift
+++ Sources/SLEssentials/Logger/Date+ToString.swift
@@ -1,6 +1,6 @@
 //
 //  Date.swift
-// 
+//
 //
 //  Created by Milos Stankovic on 20.7.22..
 //  Copyright © 2022 SwiftyLabs. All rights reserved.
--- Sources/SLEssentials/Logger/Logger.swift
+++ Sources/SLEssentials/Logger/Logger.swift
@@ -1,6 +1,6 @@
 //
 //  Logger.swift
-// 
+//
 //
 //  Created by Milos Stankovic on 20.7.22..
 //  Copyright © 2022 SwiftyLabs. All rights reserved.
