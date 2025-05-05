4a636671b8a140f2fd6217e20f27f536e41df806
Slobodan Ristic <slobodan.ristic@swiftylabs.io>
2025-05-05 13:31:58 +0200

    Updated package.swift to support async/await

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "SLEssentials",
 	platforms: [
-		.iOS(.v11),
+		.iOS(.v13),
 		.tvOS(.v15)
 	],
 	products: [
@@ -16,14 +16,14 @@ let package = Package(
 		)
 	],
 	dependencies: [
-		.package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
+		.package(url: "https://github.com/ashleymills/Reachability.swift.git", .upToNextMajor(from: "5.1.0")),
 		.package(url: "https://github.com/roberthein/TinyConstraints.git", .upToNextMajor(from: "4.0.2"))
 	],
 	targets: [
 		.target(
 			name: "SLEssentials",
 			dependencies: [
-				.product(name: "Alamofire", package: "Alamofire"),
+				.product(name: "Reachability", package: "Reachability.swift"),
 				.product(name: "TinyConstraints", package: "TinyConstraints")
 			],
 			path: "Sources"
